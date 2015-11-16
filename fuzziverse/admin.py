6d4503d07c23d68380afb071f21aa2cda2916089
Jacek Wielemborek <d33tah@gmail.com>
2015-11-16 11:14:36 +0100

    Add simple admin for Application.

--- fuzziverse/admin.py
+++ fuzziverse/admin.py
@@ -1,1 +1,9 @@
 from fuzziverse.user_admin import user_admin_site
+
+from django.contrib import admin
+
+from fuzziverse.models import Application
+
+class ApplicationAdmin(admin.ModelAdmin):
+    pass
+user_admin_site.register(Application, ApplicationAdmin)
